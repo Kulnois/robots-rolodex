5420c0020d24b964fd597177f8d4a2bf822cc9de
Kulnois <rasecmen58@hotmail.com>
2020-08-15 16:42:10 -0500

    Add structure components: Card, CardList, SearchBox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
 class App extends React.Component {
     render() {
-        return (<div>Hello Robots Rolodex</div>)
+        return (
+            <div>
+                <h1>Hello Robots Rolodex</h1>
+                <SearchBox />
+                <CardList />
+
+            </div>
+        )
     }
 }
 
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './card.styles.css';
+
+export const Card = () => (
+    <div>CARD</div>
+);
