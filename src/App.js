1d53fe0422c0e624d7d3e1999a431ec224ec6145
Kulnois <rasecmen58@hotmail.com>
2020-08-15 16:51:59 -0500

    Add fetch API to get the robots

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            robots: []
+        };
+    }
+
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello Robots Rolodex</h1>
                 <SearchBox />
                 <CardList />
+                {this.state.robots.map(robot => (
+                    <div key={robot.id}>{robot.name}</div>
+                ))}
 
             </div>
         )
