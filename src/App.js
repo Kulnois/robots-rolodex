faa9a020a61eacd3061d3ac1dc2282e4d8ba66f1
Kulnois <rasecmen58@hotmail.com>
2020-08-15 20:53:20 -0500

    The method of filtering robots and React Hooks was implemented in the app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.css';
 
-class App extends React.Component {
 
-    constructor() {
-        super();
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');    
+    const [errorMessage, setErrorMessage] = useState(null);
 
-        this.state = {
-            robots: []
-        };
-    }
-
-
-    componentDidMount() {
+    const fetchRobots = () => {        
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}))
-    }
+            .then(users => users.length > 0 ? setRobots(users) : setErrorMessage('No se encontraron resultados'))
+            .catch(error => setErrorMessage(`Error de conexión, por favor compruebe su conexión a internet --${error.message}--`))
+    };
 
-    render() {
-        return (
-            <div>
-                <h1>Robots Rolodex</h1>
-                <SearchBox />
-                <CardList robots={this.state.robots} />
+    const handleChange = (event) => {
+        setSearchField(event.target.value);
+    };
 
-            </div>
-        )
-    }
+    useEffect(() => {
+        fetchRobots();
+    }, []);    
+
+    const filteredRobots = () => {
+        if (robots.length > 0) {
+            return robots.filter(robot =>
+                robot.name.toLowerCase().includes(searchField.trim().toLowerCase())
+            )
+        }
+    };
+
+    const renderContent = () => {
+        if (errorMessage) {
+            return <h3>{errorMessage}</h3>;
+        } else if (robots.length > 0 && filteredRobots().length > 0) {
+            return <CardList robots={filteredRobots()} />;
+        } else if (robots.length > 0 && !errorMessage && filteredRobots().length === 0) {        
+            return <h3>Resultado no encontrado pruebe con otro nombre de robot.</h3>;
+        } else {
+            return <h3>Buscando robots...</h3>;
+        }
+    };
+
+    return (
+        <div>
+            <h1>Robots Rolodex</h1>
+            <SearchBox
+                placeholder="Buscar Robots"
+                handleChange={handleChange}
+            />
+            {renderContent()}
+        </div>
+    );
 }
 
 export default App;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import './search-box.styles.css';
 
 
-export const SearchBox = () => (
-    <input type="search" className="search" placeholder="Buscar Robot" />
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input 
+        type="search" 
+        className="search" 
+        placeholder={placeholder} 
+        onChange={handleChange} />
 );
