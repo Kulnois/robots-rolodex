fb273f81369dd93bfdbc5d1e4b8579c2b1fb6c60
Kulnois <rasecmen58@hotmail.com>
2020-08-15 22:04:31 -0500

    Updates

---
+++ precache-manifest.0de9c0fe575a3cd3bc11c908e6a9f4b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5f8f106809b4d3b16f453fba980e4f",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "bd4566aecd9de0d7e4e4",
+    "url": "/robots-rolodex/static/css/main.dc0538c1.chunk.css"
+  },
+  {
+    "revision": "b38f08bace7a0a9bac24",
+    "url": "/robots-rolodex/static/js/2.e5039217.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.e5039217.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4566aecd9de0d7e4e4",
+    "url": "/robots-rolodex/static/js/main.dce85dad.chunk.js"
+  },
+  {
+    "revision": "5851d6b46a5013cd692f",
+    "url": "/robots-rolodex/static/js/runtime-main.c47d174d.js"
+  }
+]);
